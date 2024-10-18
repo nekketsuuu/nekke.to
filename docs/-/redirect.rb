e23f82ba3ee3435a035f2df5e20853b324db402a
nekketsuuu <nekketsuuu@users.noreply.github.com>
2024-10-18 23:24:35 +0900

    Ignore trailing slash

--- docs/-/redirect.rb
+++ docs/-/redirect.rb
@@ -2,7 +2,7 @@ require 'js'
 
 DEFAULT_TARGET = 'https://github.com/nekketsuuu/nek-link'
 
-current = JS.global[:window][:location][:pathname].to_s
+current = JS.global[:window][:location][:pathname].to_s.chomp('/')
 target = {"/"=>"https://github.com/nekketsuuu"}.fetch(current, DEFAULT_TARGET)
 
 puts({ current:, target: })
